2bd1ec524ae022c5940001fe03beae3e20b1386b
Arsenic <danielplanteskole@gmail.com>
2025-09-11 19:02:41 +0300

    fixed last commit

--- GameOffsets/Objects/States/AreaLoadingStateOffset.cs
+++ GameOffsets/Objects/States/AreaLoadingStateOffset.cs
@@ -6,8 +6,8 @@ namespace GameOffsets.Objects.States
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
     public struct AreaLoadingStateOffset
     {
-        [FieldOffset(0x4E0)] public int IsLoading; // save the structure data before, during and after area change loading screen
-        [FieldOffset(0xA70)] public uint TotalLoadingScreenTimeMs; // increases when area changes
-        [FieldOffset(0xB00)] public IntPtr CurrentAreaDetailsPtr; // contains area name.
+        [FieldOffset(0x660)] public int IsLoading; // save the structure data before, during and after area change loading screen
+        [FieldOffset(0xDB8)] public uint TotalLoadingScreenTimeMs; // increases when area changes
+        [FieldOffset(0xE50)] public IntPtr CurrentAreaDetailsPtr; // contains area name.
     }
 }
