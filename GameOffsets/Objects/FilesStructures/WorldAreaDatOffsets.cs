16c1630ede77d83cc0a245b506e53bffbc36d54f
Arsenic <danielplanteskole@gmail.com>
2025-09-12 17:55:49 +0300

    Update WorldAreaDatOffsets.cs

--- GameOffsets/Objects/FilesStructures/WorldAreaDatOffsets.cs
+++ GameOffsets/Objects/FilesStructures/WorldAreaDatOffsets.cs
@@ -8,7 +8,7 @@
     {
         [FieldOffset(0x00)] public IntPtr IdPtr;
         [FieldOffset(0x08)] public IntPtr NamePtr;
-        [FieldOffset(0x20)] public int Act;
+        [FieldOffset(0x10)] public int Act;
         [FieldOffset(0x14)] public bool IsTown;
         [FieldOffset(0x15)] public bool HasWaypoint;
     }
